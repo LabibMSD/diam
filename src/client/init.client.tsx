ed322774dbf4fcde1533121de2cb36371ada8c60
LabibMSD <labib.msd.3001@gmail.com>
2026-09-16 11:50:38 +0700

    feat: wrap App component in ReflexProvider for enhanced state management

--- src/client/init.client.tsx
+++ src/client/init.client.tsx
@@ -2,8 +2,16 @@ import React, { StrictMode } from "@rbxts/react";
 import { createPortal, createRoot } from "@rbxts/react-roblox";
 import { Players } from "@rbxts/services";
 import { App } from "./ui/app";
+import { ReflexProvider } from "@rbxts/react-reflex";
+import { rootProducer } from "./store";
 
 const playerGui = Players.LocalPlayer.WaitForChild("PlayerGui") as PlayerGui
 const root = createRoot(new Instance("Folder"));
 
-root.render(<StrictMode>{createPortal(<screengui><App /></screengui>, playerGui)}</StrictMode>);
+root.render(
+	<StrictMode>
+		<ReflexProvider producer={rootProducer}>
+			{createPortal(<App />, playerGui)}
+		</ReflexProvider>
+	</StrictMode>,
+);
