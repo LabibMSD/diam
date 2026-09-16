12d26bec05c6225f158f8828fe15cc0c047e5edc
LabibMSD <labib.msd.3001@gmail.com>
2026-09-16 13:23:40 +0700

    feat: update ESLint formatter to Prettier and add @rbxts/ui-labs dependency

---
+++ src/client/ui/app.story.tsx
@@ -0,0 +1,14 @@
+import React from "@rbxts/react";
+import ReactRoblox from "@rbxts/react-roblox";
+import { CreateReactStory } from "@rbxts/ui-labs";
+import { App } from "./app";
+
+export = CreateReactStory(
+	{
+		react: React,
+		reactRoblox: ReactRoblox,
+	},
+	() => {
+		return <App />;
+	},
+);
