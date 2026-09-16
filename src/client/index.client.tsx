ed01c1aa6b6df12730e9beb167566e05603a48a1
LabibMSD <labib.msd.3001@gmail.com>
2026-09-16 14:01:44 +0700

    refactor: update import path for App component and clean up index.client.tsx

--- src/client/index.client.tsx
+++ src/client/index.client.tsx
@@ -1,17 +1,15 @@
 import React, { StrictMode } from "@rbxts/react";
 import { createPortal, createRoot } from "@rbxts/react-roblox";
 import { Players } from "@rbxts/services";
-import { App } from "./ui/app";
 import { ReflexProvider } from "@rbxts/react-reflex";
 import { rootProducer } from "./store";
+import { App } from "./ui/app.ui";
 
-const playerGui = Players.LocalPlayer.WaitForChild("PlayerGui") as PlayerGui
+const playerGui = Players.LocalPlayer.WaitForChild("PlayerGui") as PlayerGui;
 const root = createRoot(new Instance("Folder"));
 
 root.render(
 	<StrictMode>
-		<ReflexProvider producer={rootProducer}>
-			{createPortal(<App />, playerGui)}
-		</ReflexProvider>
+		<ReflexProvider producer={rootProducer}>{createPortal(<App />, playerGui)}</ReflexProvider>
 	</StrictMode>,
 );
