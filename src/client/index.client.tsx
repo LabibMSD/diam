a2e45082dcdc7796f03ca42f77ceb9df073ad8fc
LabibMSD <labib.msd.3001@gmail.com>
2026-09-18 00:27:44 +0700

    refactor: consolidate Flamework initialization in index.client.tsx and index.server.ts

--- src/client/index.client.tsx
+++ src/client/index.client.tsx
@@ -1,9 +1,16 @@
+import { Flamework } from "@flamework/core";
 import React, { StrictMode } from "@rbxts/react";
 import { createPortal, createRoot } from "@rbxts/react-roblox";
 import { Players } from "@rbxts/services";
 import { ReflexProvider } from "@rbxts/react-reflex";
 import { rootProducer } from "./store";
 import { App } from "./ui/app.ui";
+
+Flamework.addPaths("src/client/components");
+Flamework.addPaths("src/client/controllers");
+Flamework.addPaths("src/shared/components");
+
+Flamework.ignite();
 
 const playerGui = Players.LocalPlayer.WaitForChild("PlayerGui") as PlayerGui;
 const root = createRoot(new Instance("Folder"));
